71ae89841c67c6d43be167032711d0f59ca54a6f
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2024-12-08 16:28:39 +0900

    fix: ロックタイミングの調整

--- go/chair_handlers.go
+++ go/chair_handlers.go
@@ -260,7 +260,6 @@ func bulkInsertCoordinates(ctx context.Context, tx *sqlx.Tx, coordinates []*Coor
         INSERT INTO chair_locations (id, chair_id, latitude, longitude, created_at)
         VALUES (:id, :chair_id, :latitude, :longitude, :created_at)`
 
-	// 各 CoordinateBF を NamedExec 用のマップに変換
 	if _, err := tx.NamedExecContext(ctx, query, coordinates); err != nil {
 		log.Printf("Failed to insert location: %v", err)
 		return err
@@ -284,16 +283,14 @@ func startBufferProcessor() {
 
 // バッファ内のデータを保存し、関連する rides を処理
 func saveBufferedCoordinates(ctx context.Context) {
-	bufferLock.Lock()
-	defer bufferLock.Unlock()
-
 	if len(coordinateBuf) == 0 {
 		return
 	}
 
-	// バッファの内容をコピーしてクリア
+	bufferLock.Lock()
 	toSave := coordinateBuf
 	coordinateBuf = nil
+	bufferLock.Unlock()
 
 	// トランザクションを開始
 	tx, err := db.Beginx()
